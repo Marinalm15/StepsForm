5f5e042f48e9171ff6391e16b2a78237abe1d8a0
Marinalm15 <marinalm15@hotmail.com>
2024-01-14 12:50:19 -0300

    create the sidebar

---
+++ src/Layout/Sidebar/index.tsx
@@ -0,0 +1,49 @@
+import { NumberCircleFour, NumberCircleOne, NumberCircleThree, NumberCircleTwo } from "@phosphor-icons/react";
+import { FistTitle, SecondTitle, SidebarContainer, StepNumber, StepWrapper, Steps } from "./styles";
+
+export function Sidebar() {
+    return (
+        <SidebarContainer>
+           <Steps>
+                <StepNumber>
+                <NumberCircleOne size={32} />
+                </StepNumber>
+                <StepWrapper>
+                    <FistTitle>Step 2</FistTitle>
+                    <SecondTitle>YOUR INFO</SecondTitle>
+                </StepWrapper>
+           </Steps>
+
+           <Steps>
+                <StepNumber>
+                    <NumberCircleTwo size={32} />
+                </StepNumber>
+                <StepWrapper>
+                    <FistTitle>Step 2</FistTitle>
+                    <SecondTitle>SELECT PLAN</SecondTitle>
+                </StepWrapper>
+           </Steps>
+
+           <Steps>
+           <StepNumber>
+                    <NumberCircleThree size={32} />
+                </StepNumber>
+                <StepWrapper>
+                    <FistTitle>Step 3</FistTitle>
+                    <SecondTitle>ADD-ONS</SecondTitle>
+                </StepWrapper>
+           </Steps>
+
+           <Steps>
+           <StepNumber>
+                    <NumberCircleFour size={32} />
+                </StepNumber>
+                <StepWrapper>
+                    <FistTitle>Step 4</FistTitle>
+                    <SecondTitle>SUMMARY</SecondTitle>
+                </StepWrapper>
+           </Steps>
+
+        </SidebarContainer>
+    )
+}
---
+++ src/Layout/Sidebar/styles.ts
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const SidebarContainer = styled.div`
+    position: absolute;
+    margin-top: 4rem;
+`
+
+export const StepNumber = styled.div`
+    color: #fff;
+
+`
+
+export const FistTitle = styled.div`
+    color: #fff;
+    opacity: 0.5;
+`
+export const SecondTitle = styled.div`
+    color: #fff;
+
+`
+export const Steps = styled.div`
+    color: #fff;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 1rem;
+    margin-bottom: 2rem;
+`
+
+export const StepWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+
+`
